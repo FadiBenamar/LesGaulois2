cbe8ebf8b41c5a37a0625d2789547fe1355f6eea
FadiBenamar <fadi.benamar@univ-tlse3.fr>
2023-09-26 19:59:23 +0200

    TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,13 +1,42 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Village {
 	private String nom;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
 
 	public Village(String nom) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
 	}
 
 	public String getNom() {
 		return nom;
 	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois < nbVillageoisMaximum) {
+			villageois[nbVillageois] = gaulois;
+			nbVillageois++;
+		}
+	}
+
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois];
+	}
+
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles");
+		Gaulois gaulois = village.trouverHabitant(1);
+//		Chef chef = new Chef("Abraracourcix", 6, village);
+//		Gaulois asterix = new Gaulois("Astérix", 8);
+//		Gaulois gaulois1 = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+
+	}
+
+
 }
